0c2f480b6915bfa25741a5430f9553135741d421
knezn <knez@mail.com>
2023-10-31 10:23:08 +0100

    first test goofying around

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('test')
-print('test2')
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def home_test():
+    return "<p>hi</p>"
+
+if __name__ == '__main__':
+    app.run()
