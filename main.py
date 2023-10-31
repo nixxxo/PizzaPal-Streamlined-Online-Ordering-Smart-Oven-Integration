4708e1482fc29e9840415c494d5ff2971729869a
knezn <knez@mail.com>
2023-10-31 11:19:44 +0100

    first test goofying around

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
-
-@app.route('/')
-def home_test():
-    return "<p>hi</p>"
-
-if __name__ == '__main__':
-    app.run()
